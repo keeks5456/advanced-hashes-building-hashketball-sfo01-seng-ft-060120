1e444c4e0d9dfb8e3bc6182a2195be739ea8954e
Akiko Green <akikogreen08@gmail.com>
2020-05-05 20:13:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,10 @@
-# Write your code here!
+def game_hash
+  :home = {
+    :team_name => " ", #player names
+    :colors => [" "],  team color
+    :players => [ { } ] #player's stats
+  }
+  :away = {
+    
+  }
+end
