c89e1b8660ff0e7b1f9c13f0ee5c3c4614692b45
Akiko Green <akikogreen08@gmail.com>
2020-05-05 20:42:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,29 +5,29 @@ def game_hash
       :colors => ["black", "white"] # team color Array
       :players => [
         {
-          :player_name => "",
-          :number => ,
-          :shoe => 
-          :points =>
-          :rebonds =>
-          :assists =>
-          :steals =>
-          :blocks => 
-          :slam_dunks =>
+          :player_name => "Alan Anderson",
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebonds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1,
         },
         {
-          :player_name => "",
-          :number => ,
-          :shoe => 
-          :points =>
-          :rebonds =>
-          :assists =>
-          :steals =>
-          :blocks => 
-          :slam_dunks =>
+          :player_name => "Reggie Evans",
+          :number => 30,
+          :shoe => 14,
+          :points => 12,
+          :rebonds => 12,
+          :assists => 12,
+          :steals => 12,
+          :blocks => 12,
+          :slam_dunks => 7,
         },
         {
-          :player_name => "",
+          :player_name => "Brook Lopez",
           :number => ,
           :shoe => 
           :points =>
