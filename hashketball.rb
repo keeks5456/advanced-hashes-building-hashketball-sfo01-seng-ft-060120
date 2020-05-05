58014f69b917b92b0ee16250e5b7856f06eb0dba
Akiko Green <akikogreen08@gmail.com>
2020-05-05 20:38:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,10 +14,114 @@ def game_hash
           :steals =>
           :blocks => 
           :slam_dunks =>
-        }
+        },
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => 
+          :points =>
+          :rebonds =>
+          :assists =>
+          :steals =>
+          :blocks => 
+          :slam_dunks =>
+        },
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => 
+          :points =>
+          :rebonds =>
+          :assists =>
+          :steals =>
+          :blocks => 
+          :slam_dunks =>
+        },
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => 
+          :points =>
+          :rebonds =>
+          :assists =>
+          :steals =>
+          :blocks => 
+          :slam_dunks =>
+        },
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => 
+          :points =>
+          :rebonds =>
+          :assists =>
+          :steals =>
+          :blocks => 
+          :slam_dunks =>
+        },
         ]#players Array of Hashes
     },
-    :away = {}
+    :away = {
+      {
+      :team_name => "Brooklyn Nets" #team name String
+      :colors => ["black", "white"] # team color Array
+      :players => [
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => 
+          :points =>
+          :rebonds =>
+          :assists =>
+          :steals =>
+          :blocks => 
+          :slam_dunks =>
+        },
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => 
+          :points =>
+          :rebonds =>
+          :assists =>
+          :steals =>
+          :blocks => 
+          :slam_dunks =>
+        },
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => 
+          :points =>
+          :rebonds =>
+          :assists =>
+          :steals =>
+          :blocks => 
+          :slam_dunks =>
+        },
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => 
+          :points =>
+          :rebonds =>
+          :assists =>
+          :steals =>
+          :blocks => 
+          :slam_dunks =>
+        },
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => 
+          :points =>
+          :rebonds =>
+          :assists =>
+          :steals =>
+          :blocks => 
+          :slam_dunks =>
+        },
+    }
   }
 end 
 
