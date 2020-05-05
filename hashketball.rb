89fb2af5cdbc1add94f4a231d810a86bb3d9b482
Akiko Green <akikogreen08@gmail.com>
2020-05-05 20:36:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,17 @@ def game_hash
       :team_name => "Brooklyn Nets" #team name String
       :colors => ["black", "white"] # team color Array
       :players => [
-        {:player_name => "" }
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => 
+          :points =>
+          :rebonds =>
+          :assists =>
+          :steals =>
+          :blocks => 
+          :slam_dunks =>
+        }
         ]#players Array of Hashes
     },
     :away = {}
